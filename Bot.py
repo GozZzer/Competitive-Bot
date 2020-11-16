ee17ac9a67c262f727cd26ccf52c1331f4fe54df
GozZzer <gozzzer432@gmail.com>
2020-11-16 17:53:11 +0100

    1.1

--- Bot.py
+++ Bot.py
@@ -20,4 +20,4 @@ for filename in os.listdir('cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run(token)
+client.run(os.eviron["token"])
--- variables.py
+++ variables.py
@@ -4,6 +4,4 @@ prefix = "elo!"
 
 client = commands.Bot(command_prefix=prefix)
 
-token = 'Nzc3MTI2Mzk5ODg2MDk4NDMy.X6-5XA.3DBOuVPR-AnsJF-DBBkXwt1BSLs'
-
 guild_id = '776519656168620053'
